9d9ba2312c45593e5e72bf1bcc4c748efad0ab65
Sam Kent <sam.john.kent@gmail.com>
2022-07-04 21:02:22 +0100

    Simplify example

--- main.cpp
+++ main.cpp
@@ -3,24 +3,22 @@
 #include <vector>
 #include <cstdlib>
 
-#include "libraries/pico_display_2/pico_display_2.hpp"
-#include "drivers/st7789/st7789.hpp"
-#include "libraries/pico_graphics/pico_graphics.hpp"
-#include "rgbled.hpp"
-#include "button.hpp"
-
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
 #include "lwip/pbuf.h"
 #include "lwip/tcp.h"
 
+#include "ws.h"
+
 #define TEST_TCP_SERVER_IP "REMOVED"
 #define TCP_PORT 8082
 
-#define DEBUG_printf printf
-#define POLL_TIME_S 5
 #define BUF_SIZE 2048
+
+#define TCP_DISCONNECTED 0
+#define TCP_CONNECTING   1
+#define TCP_CONNECTED    2
 
 typedef struct TCP_CLIENT_T_ {
     struct tcp_pcb *tcp_pcb;
@@ -30,69 +28,46 @@ typedef struct TCP_CLIENT_T_ {
     int buffer_len;
     int rx_buffer_len;
     int sent_len;
-    bool connected;
+    int connected;
 } TCP_CLIENT_T;
 
-using namespace pimoroni;
-
-ST7789 st7789(320, 240, ROTATE_0, false, get_spi_pins(BG_SPI_FRONT));
-PicoGraphics_PenRGB332 graphics(st7789.width, st7789.height, nullptr);
-
-RGBLED led(PicoDisplay2::LED_R, PicoDisplay2::LED_G, PicoDisplay2::LED_B);
-
-Button button_a(PicoDisplay2::A);
-Button button_b(PicoDisplay2::B);
-Button button_x(PicoDisplay2::X);
-Button button_y(PicoDisplay2::Y);
-  
 char text[BUF_SIZE] = "Disconnected";
-
-// HSV Conversion expects float inputs in the range of 0.00-1.00 for each channel
-// Outputs are rgb in the range 0-255 for each channel
-void from_hsv(float h, float s, float v, uint8_t &r, uint8_t &g, uint8_t &b) {
-  float i = floor(h * 6.0f);
-  float f = h * 6.0f - i;
-  v *= 255.0f;
-  uint8_t p = v * (1.0f - s);
-  uint8_t q = v * (1.0f - f * s);
-  uint8_t t = v * (1.0f - (1.0f - f) * s);
-
-  switch (int(i) % 6) {
-    case 0: r = v; g = t; b = p; break;
-    case 1: r = q; g = v; b = p; break;
-    case 2: r = p; g = v; b = t; break;
-    case 3: r = p; g = q; b = v; break;
-    case 4: r = t; g = p; b = v; break;
-    case 5: r = v; g = p; b = q; break;
-  }
-}
 
 static err_t tcp_client_sent(void *arg, struct tcp_pcb *tpcb, u16_t len) {
     TCP_CLIENT_T *state = (TCP_CLIENT_T*)arg;
-    DEBUG_printf("tcp_client_sent %u\n", len);
+    printf("tcp_client_sent %u\n", len);
     return ERR_OK;
 }
 
 static err_t tcp_client_connected(void *arg, struct tcp_pcb *tpcb, err_t err) {
     TCP_CLIENT_T *state = (TCP_CLIENT_T*)arg;
     if (err != ERR_OK) {
-        printf("connect failed %d\n", err);
+        printf("Connect failed %d\n", err);
         // return tcp_result(arg, err);
     }
-    state->connected = true;
+ 
+    // Write HTTP GET Request with Websocket upgrade 
+    state->buffer_len = sprintf((char*)state->buffer, "GET / HTTP/1.1\r\nHost: REMOVED:8082\r\nUpgrade: websocket\r\nConnection: Upgrade\r\nSec-WebSocket-Key: x3JJHMbDL1EzLkh9GBhXDw==\r\nSec-WebSocket-Protocol: chat, superchat\r\nSec-WebSocket-Version: 13\r\n\r\n");
+    err = tcp_write(state->tcp_pcb, state->buffer, state->buffer_len, TCP_WRITE_FLAG_COPY);
+
+    state->connected = TCP_CONNECTED;
+
+    printf("Connected\r\n");
     return ERR_OK;
 }
 
 static err_t tcp_client_poll(void *arg, struct tcp_pcb *tpcb) {
-    DEBUG_printf("tcp_client_poll\n");
+    printf("tcp_client_poll\n");
     return ERR_OK;
 }
 
 static void tcp_client_err(void *arg, err_t err) {
+    TCP_CLIENT_T *state = (TCP_CLIENT_T*)arg;
+    state->connected = TCP_DISCONNECTED;
     if (err != ERR_ABRT) {
-        DEBUG_printf("tcp_client_err %d\n", err);
+        printf("tcp_client_err %d\n", err);
     } else {
-        DEBUG_printf("tcp_client_err abort %d\n", err);
+        printf("tcp_client_err abort %d\n", err);
     }
 }
 
@@ -107,12 +82,13 @@ static err_t tcp_client_close(void *arg) {
         tcp_err(state->tcp_pcb, NULL);
         err = tcp_close(state->tcp_pcb);
         if (err != ERR_OK) {
-            DEBUG_printf("close failed %d, calling abort\n", err);
+            printf("close failed %d, calling abort\n", err);
             tcp_abort(state->tcp_pcb);
             err = ERR_ABRT;
         }
         state->tcp_pcb = NULL;
     }
+    state->connected = TCP_DISCONNECTED;
     return err;
 }
 
@@ -130,7 +106,6 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
 
     if(p == NULL) {
         // Close
-        printf("received close \r\n");
         tcp_client_close(arg);
         return ERR_OK;
     }
@@ -138,8 +113,10 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
     if (p->tot_len > 0) {
         for (struct pbuf *q = p; q != NULL; q = q->next) {
             if((state->rx_buffer_len + q->len) < BUF_SIZE) {
-                memcpy(state->rx_buffer + state->rx_buffer_len, q->payload, q->len);
-                state->rx_buffer_len += q->len;
+                WebsocketPacketHeader_t header;
+                WS::ParsePacket(&header, (char *)q->payload, q->len);
+                memcpy(state->rx_buffer + state->rx_buffer_len, (uint8_t *)q->payload + header.start, header.length);
+                state->rx_buffer_len += header.length;
             }
         }
         printf("tcp_recved \r\n");
@@ -150,37 +127,39 @@ err_t tcp_client_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err
     return ERR_OK;
 }
 
+
+static err_t connect(void *arg) {
+  TCP_CLIENT_T *state = (TCP_CLIENT_T*)arg;
+    
+  if(state->connected != TCP_DISCONNECTED) return ERR_OK;
+  
+  state->tcp_pcb = tcp_new_ip_type(IP_GET_TYPE(&state->remote_addr));
+  if (!state->tcp_pcb) {
+      return false;
+  }
+
+  tcp_arg(state->tcp_pcb, state);
+  tcp_poll(state->tcp_pcb, tcp_client_poll, 1);
+  tcp_sent(state->tcp_pcb, tcp_client_sent);
+  tcp_recv(state->tcp_pcb, tcp_client_recv);
+  tcp_err(state->tcp_pcb, tcp_client_err);
+
+  state->buffer_len = 0;
+  // cyw43_arch_lwip_begin/end should be used around calls into lwIP to ensure correct locking.
+  // You can omit them if you are in a callback from lwIP. Note that when using pico_cyw_arch_poll
+  // these calls are a no-op and can be omitted, but it is a good practice to use them in
+  // case you switch the cyw43_arch type later.
+  cyw43_arch_lwip_begin();
+  state->connected = TCP_CONNECTING;
+  err_t err = tcp_connect(state->tcp_pcb, &state->remote_addr, TCP_PORT, tcp_client_connected);
+  cyw43_arch_lwip_end();
+        
+  return ERR_ABRT;
+}
+
 int main() {
 
   stdio_usb_init();
-
-  st7789.set_backlight(255);
-
-  struct pt {
-    float      x;
-    float      y;
-    uint8_t    r;
-    float     dx;
-    float     dy;
-    uint16_t pen;
-  };
-
-  std::vector<pt> shapes;
-  for(int i = 0; i < 200; i++) {
-    pt shape;
-    shape.x = rand() % graphics.bounds.w;
-    shape.y = rand() % graphics.bounds.h;
-    shape.r = (rand() % 10) + 3;
-    shape.dx = float(rand() % 255) / 64.0f;
-    shape.dy = float(rand() % 255) / 64.0f;
-    shape.pen = graphics.create_pen(rand() % 255, rand() % 255, rand() % 255);
-    shapes.push_back(shape);
-  }
-
-  Point text_location(0, 0);
-
-  Pen BG = graphics.create_pen(120, 40, 60);
-  Pen WHITE = graphics.create_pen(255, 255, 255);
 
   // Connect to network
   char ssid[] = "REMOVED";
@@ -197,99 +176,46 @@ int main() {
     return 1;
   }
 
+  // Set up TCP
   TCP_CLIENT_T *state = (TCP_CLIENT_T *)calloc(1, sizeof(TCP_CLIENT_T));
   if (!state) {
-      DEBUG_printf("failed to allocate state\n");
+      printf("failed to allocate state\n");
       return false;
   }
   ip4addr_aton(TEST_TCP_SERVER_IP, &state->remote_addr);
 
-  DEBUG_printf("Connecting to %s port %u\n", ip4addr_ntoa(&state->remote_addr), TCP_PORT);
-  state->tcp_pcb = tcp_new_ip_type(IP_GET_TYPE(&state->remote_addr));
-  if (!state->tcp_pcb) {
-      DEBUG_printf("failed to create pcb\n");
-      return false;
-  }
-
-  tcp_arg(state->tcp_pcb, state);
-  tcp_poll(state->tcp_pcb, tcp_client_poll, POLL_TIME_S * 2);
-  tcp_sent(state->tcp_pcb, tcp_client_sent);
-  tcp_recv(state->tcp_pcb, tcp_client_recv);
-  tcp_err(state->tcp_pcb, tcp_client_err);
-
-  state->buffer_len = 0;
-
-  // cyw43_arch_lwip_begin/end should be used around calls into lwIP to ensure correct locking.
-  // You can omit them if you are in a callback from lwIP. Note that when using pico_cyw_arch_poll
-  // these calls are a no-op and can be omitted, but it is a good practice to use them in
-  // case you switch the cyw43_arch type later.
-  cyw43_arch_lwip_begin();
-  err_t err = tcp_connect(state->tcp_pcb, &state->remote_addr, TCP_PORT, tcp_client_connected);
-  cyw43_arch_lwip_end();
+  printf("Connecting to %s port %u\n", ip4addr_ntoa(&state->remote_addr), TCP_PORT);
+  connect(state);
 
   uint32_t sw_timer;
 
   while(true) {
 
-    sprintf(text, "%.*s", state->rx_buffer_len, (char*)state->rx_buffer);
-
-    if(button_a.raw()) text_location.x -= 1;
-    if(button_b.raw()) text_location.x += 1;
-
-    if(button_x.raw()) text_location.y -= 1;
-    if(button_y.raw()) text_location.y += 1;
-  
-    graphics.set_pen(BG);
-    graphics.clear();
-
-    for(auto &shape : shapes) {
-      shape.x += shape.dx;
-      shape.y += shape.dy;
-      if((shape.x - shape.r) < 0) {
-        shape.dx *= -1;
-        shape.x = shape.r;
-      }
-      if((shape.x + shape.r) >= graphics.bounds.w) {
-        shape.dx *= -1;
-        shape.x = graphics.bounds.w - shape.r;
-      }
-      if((shape.y - shape.r) < 0) {
-        shape.dy *= -1;
-        shape.y = shape.r;
-      }
-      if((shape.y + shape.r) >= graphics.bounds.h) {
-        shape.dy *= -1;
-        shape.y = graphics.bounds.h - shape.r;
-      }
-
-      graphics.set_pen(shape.pen);
-      graphics.circle(Point(shape.x, shape.y), shape.r);
-
+    // Print buffer contents and reset it
+    if(state->rx_buffer_len) {
+        printf("%.*s \r\n", state->rx_buffer_len, (char*)state->rx_buffer);
+        state->rx_buffer_len = 0;
     }
+    
 
-    // Since HSV takes a float from 0.0 to 1.0 indicating hue,
-    // then we can divide millis by the number of milliseconds
-    // we want a full colour cycle to take. 5000 = 5 sec.
-    uint8_t r = 0, g = 0, b = 0;
-    from_hsv((float)millis() / 5000.0f, 1.0f, 0.5f + sinf(millis() / 100.0f / 3.14159f) * 0.5f, r, g, b);
-    led.set_rgb(r, g, b);
-
-
-    graphics.set_pen(WHITE);
-    graphics.text(text, text_location, 320);
-
-    // update screen
-    st7789.update(&graphics);
-
-    if((to_ms_since_boot(get_absolute_time()) - sw_timer) > 10000)
+    // Every second
+    if((to_ms_since_boot(get_absolute_time()) - sw_timer) > 1000)
     {
-        printf("timer \r\n");
-        // write to tcp
-        state->buffer_len = sprintf((char*)state->buffer, "GET / HTTP/1.1\r\nHost: REMOVED:8082\r\nConnection: keep-alive\r\n\r\n");
-        err_t err = tcp_write(state->tcp_pcb, state->buffer, state->buffer_len, TCP_WRITE_FLAG_COPY);
-        if (err != ERR_OK) {
-            DEBUG_printf("Failed to write data %d\n", err);
+        if(state->connected == TCP_CONNECTED) {
+            // write to tcp
+            char test[20] = "test";
+            state->buffer_len = WS::BuildPacket((char *)state->buffer, BUF_SIZE, WEBSOCKET_OPCODE_TEXT, test, sizeof(test), 1);
+            err_t err = tcp_write(state->tcp_pcb, state->buffer, state->buffer_len, TCP_WRITE_FLAG_COPY);
+            if (err != ERR_OK) {
+                printf("Failed to write data %d\n", err);
+            }
         }
+        
+        if(state->connected == TCP_DISCONNECTED) {        
+            // Reconnect
+            connect(state);
+        }
+
         sw_timer = to_ms_since_boot(get_absolute_time());
     }
 
